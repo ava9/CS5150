d1d74789f3c90ae0d457ebcc346cceca7338ea8c
ava9 <ava9@cornell.edu>
2017-03-20 01:22:33 -0400

    working mysql to kml parser for all bands - need to figure out how to color using kml styling and also need to determine whether we need to group the bands for a time slot together - so that each time slot can be color coordinated

--- php/config.php
+++ php/config.php
@@ -1,12 +1,12 @@
 <?php // ** MySQL connection settings ** //
 // database host
-define('DB_HOST', 'localhost');
+define('DB_HOST', '127.0.0.1');
 
 // database name
 define('DB_NAME', 'porchfest');
 // Your MySQL / Course Server username
-define('DB_USER', 'porchfest');
+define('DB_USER', 'root');
 // ...and password
-define('DB_PASSWORD', 'porchfest2017');
+define('DB_PASSWORD', '');
 
 ?>
