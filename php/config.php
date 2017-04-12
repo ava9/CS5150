867350cb252095fe3fcb99df995a30ea0505e22c
ajzwu8 <ajw238@cornell.edu>
2017-04-12 14:07:33 -0400

    url routing fixed?

---
+++ php/config.php
@@ -0,0 +1,14 @@
+<?php // ** MySQL connection settings ** //
+// database host
+define('DB_HOST', 'localhost:3306');
+
+// database name
+define('DB_NAME', 'porchfest');
+// Your MySQL / Course Server username
+define('DB_USER', 'root');
+// ...and password
+define('DB_PASSWORD', '');
+// also salt
+define('SALT', 'PorchfestsForever');
+
+?>
--- php/routing.php
+++ php/routing.php
@@ -10,10 +10,12 @@
     $base_url = getCurrentUri();
     // echo sprintf("Current URI: %s \n", $base_url);
     $uri_array = array_values(array_filter(explode('/', $base_url)));
-    foreach ($uri_array as $key => $value) {
-        $uri_array[$key] = ucwords(urldecode($value));
+    // foreach ($uri_array as $key => $value) {
+    //     $uri_array[$key] = ucwords(urldecode($value));
+    // }
+    if (in_array($uri_array[0], ['singleporchfest', 'editporchfest', 'bandsignup', 'editband'])) {
+        define('PORCHFEST_NAME_RAW', $uri_array[1]);
+        define('PORCHFEST_NAME_CLEAN', ucwords(urldecode($uri_array[1])));
     }
-    define('URI_ARRAY', $uri_array);
-
 ?>
 
