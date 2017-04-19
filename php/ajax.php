53a90ccd494d13c51f057aca36d93adc59e19f57
Carlos <cam468@cornell.edu>
2017-04-19 18:03:15 -0400

    some validation + fixing some things in editporchfest

--- php/ajax.php
+++ php/ajax.php
@@ -1,48 +1,44 @@
 <?php
 	require_once "config.php";
-	require_once "routing.php";
-
-	function __isset($name) {
-	    return $this->args[$name];
-	}
-
 
 	// Create connection
 	// add DB_USER and DB_PASSWORD later
 	$conn = $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
-	$sql = sprintf("SELECT PorchfestID FROM porchfests WHERE porchfests.Name = '%s'", PORCHFEST_NAME_CLEAN);
-    $result = $conn->query($sql);
-    $porchfestID = $result->fetch_assoc()['PorchfestID'];
+	// $sql = sprintf("SELECT PorchfestID FROM porchfests WHERE porchfests.Name = '%s'", 1);
+ //    $result = $conn->query($sql);
+ //    $porchfestID = $result->fetch_assoc()['PorchfestID'];
 
 	// ** editporchfest.php: form to manage porchfest 
-	if (isset($_POST['porchfestname']) && isset($_POST['porchfestlocation']) && isset($_POST['porchfestdate']) && isset($_POST['porchfestdescription']) && isset($_POST['porchfesttime']) && isset($_POST['porchfestdeadlineday'])) {
+	if (isset($_POST['porchfestname']) && isset($_POST['porchfestlocation']) && isset($_POST['porchfestdate']) && isset($_POST['porchfestdescription']) && isset($_POST['porchfesttime']) && isset($_POST['porchfestdeadlineday']) && isset($_POST['porchfestid'])) {
 		$porchfestname = htmlentities($_POST['porchfestname']);
 		$porchfestlocation = htmlentities($_POST['porchfestlocation']);
 		$porchfestdate = htmlentities($_POST['porchfestdate']);
 		$porchfestdescription = htmlentities($_POST['porchfestdescription']);
 		$porchfesttime = htmlentities($_POST['porchfesttime']);
 		$porchfestdeadlineday = htmlentities($_POST['porchfestdeadlineday']);
+		$porchfestid = $_POST['porchfestid'];
 
-		$sql = 'SELECT Deadline FROM porchfests WHERE PorchfestID=1';
+
+		$sql = 'SELECT Deadline FROM porchfests WHERE PorchfestID=' . $porchfestid;
 		$result = $conn->query($sql);
 
 		$deadline = new DateTime($result->fetch_assoc()['Deadline']);
 		list($hour, $minute) = explode(":", $porchfesttime);
 		date_time_set($deadline, intval($hour), intval($minute));
 
+		echo $porchfestdeadlineday;
+
 		list($year, $month, $day) = explode("-", $porchfestdeadlineday);
 		$deadline->setDate(intval($year), intval($month), intval($day));
 
-		$sql = "UPDATE porchfests SET Name='" . $porchfestname . "', Location='" . $porchfestlocation . "', Date='" . $porchfestdate . "', Description='" . $porchfestdescription . "', Deadline='" . $deadline->format("Y-m-d H:i:s")  . "' WHERE PorchfestID = '" . $porchfestID . "'";
-		echo $sql;
+		$sql = "UPDATE porchfests SET Name='" . $porchfestname . "', Location='" . $porchfestlocation . "', Date='" . $porchfestdate . "', Description='" . $porchfestdescription . "', Deadline='" . $deadline->format("Y-m-d H:i:s")  . "' WHERE PorchfestID = '" . $porchfestid . "'";
+
 		$result = $conn->query($sql);
 		
 		if ($result) {
-			echo ($conn->error);
 			echo "success";
 		} else {
-			print_r($conn->error);
 			echo "fail";
 		}
 
@@ -54,8 +50,6 @@
 
 		$result = $conn->query($sql);
 
-		echo $conn->error;
-
 		$sql2 = "UPDATE `bandstoporchfests` SET TimeSlotID=" . $timeslotid . " WHERE BandID=" . $bandid;
 
 		$result2 = $conn->query($sql2);
@@ -65,26 +59,28 @@
 		} else {
 			echo "no overlap";
 		}
-	} elseif (isset($_POST['timeslotstart']) && isset($_POST['timeslotend']) && isset($_POST['start']) && isset($_POST['end']) && isset($_POST['timeslotid'])) {
+	} elseif (isset($_POST['timeslotstart']) && isset($_POST['timeslotend']) && isset($_POST['start']) && isset($_POST['end']) && isset($_POST['timeslotid'])
+			&& isset($_POST['porchfestid'])) {
 		// ** editporchfest.php: update timeslot.
+		$porchfestid = $_POST['porchfestid'];
 		$timeslotid = htmlentities($_POST['timeslotid']);
 		$timeslotstart = htmlentities($_POST['timeslotstart']);
 		$timeslotend = htmlentities($_POST['timeslotend']);
-		$start = date_create_from_format('Y.m.d g:i A', htmlentities($_POST['start']));
-		$end = date_create_from_format('Y.m.d g:i A', htmlentities($_POST['end']));
+		$start = date_create_from_format('Y.m.d g:iA', htmlentities($_POST['start']));
+		$end = date_create_from_format('Y.m.d g:iA', htmlentities($_POST['end']));
 
-		$datestart = date_create_from_format('g:i A', $timeslotstart);
-		$dateend = date_create_from_format('g:i A', $timeslotend);
+		$datestart = date_create_from_format('g:iA', $timeslotstart);
+		$dateend = date_create_from_format('g:iA', $timeslotend);
 
 		$start->setTime($datestart->format('H'), $datestart->format('i'));
 		$end->setTime($dateend->format('H'), $dateend->format('i'));
 
-		$sql = "UPDATE porchfesttimeslots SET StartTime='" . $start->format('Y-m-d H:i:s') . "', EndTime='" . $end->format('Y-m-d H:i:s') . "' WHERE PorchfestID=1 AND TimeslotID=" . $timeslotid;
+		$sql = "UPDATE porchfesttimeslots SET StartTime='" . $start->format('Y-m-d H:i:s') . "', EndTime='" . $end->format('Y-m-d H:i:s') . "' WHERE PorchfestID=". $porchfestid . " AND TimeslotID=" . $timeslotid;
 
 		$result = $conn->query($sql);		
 
 		if ($result) {
-			echo 'success';
+			echo "success";
 		} else {
 			echo 'fail';
 		}
@@ -102,9 +98,10 @@
 			echo 'fail';
 		}
 
-	} elseif (isset($_GET['bandname'])) {
+	} elseif (isset($_GET['bandname']) && isset($_GET['porchfestid'])) {
 		// ** editporchfest.php: search functionality to display bands that match name.
 		$name = htmlentities($_GET['bandname']);
+		$porchfestid = $_GET['porchfestid'];
 		echo "<table class='responsive table'> <!-- begin table -->";
 		echo "<tr data-status= 'fixed'>";
 		echo "<th> Name </th>";
@@ -133,10 +130,10 @@
 	    }
 
 		if ($name == "") {
-			$sql = "SELECT * FROM `bandstoporchfests` INNER JOIN bands ON bands.BandID = bandstoporchfests.BandID WHERE PorchfestID = 1 ORDER BY bands.Name";
+			$sql = "SELECT * FROM `bandstoporchfests` INNER JOIN bands ON bands.BandID = bandstoporchfests.BandID WHERE PorchfestID =" . $porchfestid . " ORDER BY bands.Name";
 
 		} else {
-			$sql = "SELECT * FROM `bandstoporchfests` INNER JOIN bands ON bands.BandID = bandstoporchfests.BandID WHERE PorchfestID = 1 AND bands.Name LIKE '%" . $name . "%' ORDER BY bands.Name; ";
+			$sql = "SELECT * FROM `bandstoporchfests` INNER JOIN bands ON bands.BandID = bandstoporchfests.BandID WHERE PorchfestID =" . $porchfestid . " AND bands.Name LIKE '%" . $name . "%' ORDER BY bands.Name; ";
 		}
 
       	$result = $conn->query($sql);
@@ -154,6 +151,7 @@
 
 		echo '</table>';
 	} else {
+		echo 'here in ajax';
 		print_r($_POST);
 		// throw new Exception("variable not found");
 	}
