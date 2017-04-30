4ba09678ade51cb7b7beb4dcd0ec666c8b408f06
a j <a j>
2017-04-30 16:56:45 -0400

    commented/documented functions in Schedule class

--- scheduling/PorchfestScheduling/Schedule.php
+++ scheduling/PorchfestScheduling/Schedule.php
@@ -1,9 +1,28 @@
 <?php
 
+/* This is a class for a schedule object. The schedule object contains three
+ * fields: 
+ * $schedule is a hashmap of timeslots to an array of Band objects scheduled
+ *    to play in that timeslot 
+ * $timeSlotScores is a hashmap of timeslots to vectors of scores
+ *    Currently contains minimum distance between any two bands and variance
+ * $score is a Score object for the overall score of the schedule
+ *    This is the score of the worst time slot, which is checked in the
+ *    compareTo function. It is weighted evenly between the minimum distance
+ *    between any two bands in the timeslot and the variance of distances 
+ *    between the bands in the timeslot.
+ */
+
 class Schedule {
-//  $schedule; //HashMap<int timeslot, Band[] bands> 
-//  $timeSlotScores; hashmap of vectors of scores
-  
+   /*
+   * Constructs a schedule object where the $schedule hashamp has an empty 
+   * array for each of the timeslot keys, the $timeSlotScores hashmap has new
+   * initialized Score objects for each timeslot, and the overall $score is a 
+   * new Score object. 
+    * 
+   * @param timeSlotsArr    array of all timeslots available for a particular 
+   *    porchfest
+   */
   function __construct($timeSlotsArr) {
     $this->schedule = [];
     foreach ($timeSlotsArr as $timeSlotID) {
@@ -13,7 +32,12 @@ class Schedule {
     $this->score = new Score();
   }
   
-  /* Creates a copy of the schedule with score reinitialized to -1 */
+  /* Creates a copy of the schedule with score reinitialized to -1 
+   * Returns a copy of the schedule
+   * 
+   * @param timeSlotsArr    array of all timeslots available for a particular
+   *    porchfest
+   */
   function deepCopy($timeSlotsArr) {
     $copy = new Schedule($timeSlotsArr);
     $copy->schedule = [];
@@ -35,6 +59,10 @@ class Schedule {
     return $copy;
   }
 
+   /* Gets the variances for each timeslot from the $timeSlotScores vector of 
+   *    scores and creates a new array with key timeslot and value variance.
+   *  Returns this array. 
+   */
   function getTimeSlotVariances() {
     $timeSlotVariances = [];
     foreach ($this->timeSlotScores as $timeSlotID=>$score) {
@@ -43,10 +71,17 @@ class Schedule {
     return $timeSlotVariances;
   }
   
-  /* Iterates through the timeSlotVariances and find the timeslot with the
-  best/lowest variance. Try to swap with a band in there */
+  /* Iterates through the timeSlotVariances and finds the timeslot with the 
+   * best/lowest variance. Try to swap with a band in there. If not possible
+   * due to conflicts, try the next best timeslot until either the band has 
+   * been successfully swapped or all time slots have been examined. 
+   * 
+   * @param $band   the band in the worst time slot that needs to be swapped
+   *    with a band from another time slot
+   */
   function randomSwap($band) {
-    global $timeslotsPorchfests; //array of all timeslots available for a particular porchfest
+    //array of all timeslots available for a particular porchfest
+    global $timeslotsPorchfests; 
     
     $worstTimeSlot = $band->slot;
     
@@ -78,7 +113,6 @@ class Schedule {
               break;
             }
           }
-
         }
       }
     }
@@ -86,10 +120,16 @@ class Schedule {
 
   /*
    * Calculates the overall score based on the individual scores 
-   * of each of its timeslots.
+   * of each of its timeslots. This is done via the compareTo function which
+   * weights minimum distance between two bands and maximum variance of the 
+   * distances between bands in each timeslot.
+   * 
+   * Sets this schedule object's score field and makes a deep copy of the score
+   * when it is better than the current score.
    */
   function score() {
-    global $timeslotsPorchfests; //array of all timeslots available for a particular porchfest
+    //array of all timeslots available for a particular porchfest
+    global $timeslotsPorchfests; 
     foreach ($timeslotsPorchfests as $slot){
       computeVariance($slot, $this);
       computeMinDist($slot, $this);
@@ -102,14 +142,17 @@ class Schedule {
     }
   }
       
-  /* pairwise swaps to improve the schedule. recomputes the
-      variance of the two time slots that are affected by the swap */
-
-  # TODO: CHOOSE WHICH BAND TO SWAP BASED ON MIN DISTANCE OR SOMETHING LIKE THAT RATHER THAN 
-  # ALWAYS CHOOSING FROM THE WORST VARIANCE TIMESLOT
-      
+  /* Pairwise swaps to improve the schedule. Recomputes the
+   * variance and min distance between any two bands in the two time slots that
+   * are affected by the swap.
+   * 
+   * Returns an array where the first element is a boolean value true or false,
+   * and the second value is the new improved schedule or the original schedule,
+   * respectively.
+   */ 
   function improve() {
-    global $timeslotsPorchfests; //array of all timeslots available for a particular porchfest
+    //array of all timeslots available for a particular porchfest  
+    global $timeslotsPorchfests;
 
     $timeSlotVariances = $this->getTimeSlotVariances();
     arsort($timeSlotVariances);
@@ -152,12 +195,16 @@ class Schedule {
     return array(false, $this);
   }
   
-  /* Takes in band and time slot. Adds band to timeslot in this schedule */
+  /* Takes in band and time slot. 
+   * Adds band to timeslot in this schedule 
+   */
   function add($slot, $band){
     array_push($this->schedule[$slot], $band);
   }
   
-  /* Takes in band and time slot. Deletes band from timeslot in this schedule */
+  /* Takes in band and time slot. 
+   * Deletes band from timeslot in this schedule 
+   */
   function delete($slot, $band){
     for ($i = 0; $i < sizeof($this->schedule[$slot]); $i++) {
       $b = $this->schedule[$slot][$i];
@@ -169,13 +216,16 @@ class Schedule {
     $this->schedule[$slot] = array_merge($this->schedule[$slot]);
   }
   
-  /* Returns a list of all bands scheduled at specified time slot in this schedule */
+  /* Returns a list of all bands scheduled at this specified time slot in this 
+   * schedule.
+   * 
+   * @param $timeSlot   the particular time slot for which we want to see the 
+   *    currently scheduled bands.
+   */
   function getBandsAtSlot($timeSlot){
     return $this->schedule[$timeSlot];
   }
   
 }
-
-
 
 ?>
