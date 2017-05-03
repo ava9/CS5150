0d5374970f2a7c6ade864b8e9049d00ebce517d1
ava9 <ava9@cornell.edu>
2017-05-03 17:42:18 -0400

    config file

--- scheduling/PorchfestScheduling/config.php
+++ scheduling/PorchfestScheduling/config.php
@@ -1,13 +1,13 @@
 <?php // ** MySQL connection settings ** //
 // database host
-define('DB_HOST', 'localhost:8889');
+define('DB_HOST', '127.0.0.1');
 
 // database name
-define('DB_NAME', 'Porchfest');
+define('DB_NAME', 'porchfest3');
 // Your MySQL / Course Server username
-define('DB_USER', 'kevin');
+define('DB_USER', 'root');
 // ...and password
-define('DB_PASSWORD', 'password');
+define('DB_PASSWORD', '');
 // also salt
 define('SALT', 'PorchfestsForever');
 
