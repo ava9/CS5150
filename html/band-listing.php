7c847189cb6ba68683b63127bb6bc286666f56aa
Carlos <cam468@cornell.edu>
2017-05-07 22:20:47 -0400

    band listing update

--- html/band-listing.php
+++ html/band-listing.php
@@ -11,13 +11,13 @@ require_once "../php/config.php";
 $conn = $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
 # Perform the query
-$query = sprintf("SELECT bands.Name FROM `bandstoporchfests` INNER JOIN bands on bands.BandID = bandstoporchfests.BandID WHERE PorchfestID = 1 AND bands.Name LIKE '%%%s%%' LIMIT 5", $conn->real_escape_string($_GET["q"]));
+$query = sprintf("SELECT bands.Name, bands.BandID FROM `bandstoporchfests` INNER JOIN bands on bands.BandID = bandstoporchfests.BandID WHERE PorchfestID = 1 AND bands.Name LIKE '%%%s%%' LIMIT 5", $conn->real_escape_string($_GET["q"]));
 $arr = array();
 $result = $mysqli->query($query);
 
 # Collect the results
 while($obj = $result->fetch_assoc()) {
-    $arr[] = array("id" => $obj["Name"], "name" => $obj["Name"]);
+    $arr[] = array("id" => $obj["BandID"], "name" => $obj["Name"]);
 }
 
 # JSON-encode the response
