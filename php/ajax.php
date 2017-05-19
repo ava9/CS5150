574c268eae158b3370779915635a3bdee2077ca6
Rohit Biswas <rohit@dhcp-rhodes-909.redrover.cornell.edu>
2017-05-18 22:23:05 -0400

    move ajax

--- php/ajax.php
+++ php/ajax.php
@@ -1,6 +1,6 @@
 <?php
-	require_once __DIR__."../../config.php";
-	require_once "sort.php";
+	require_once __DIR__."/../config.php";
+	require_once "modules/sort.php";
 
     // Create connection
     $conn = $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
@@ -55,7 +55,7 @@
 		$isPub = $getPubResult->fetch_assoc()['Published'];
 		if (!$isPub) {
 			ob_start();
-			require_once '../scheduling/updateMap.php';
+			require_once 'scheduling/updateMap.php';
 			ob_end_clean();
 		// if published, set all flags to 0
 		} else {
@@ -73,7 +73,7 @@
 	} elseif (isset($_POST['schedule'])) {
 		// ** editporchfest.php: SCHEDULE: run scheduling algorithm
 		ob_start();
-		require_once '../scheduling/main.php';
+		require_once 'scheduling/main.php';
 		ob_end_clean();
 
 		$sql = 'SELECT scheduled FROM porchfests WHERE PorchfestID=' . $_POST['porchfestid'];
@@ -137,7 +137,7 @@
 		
 		if ($result) {
 			ob_start();
-			require_once '../scheduling/updateMap.php';
+			require_once 'scheduling/updateMap.php';
 			ob_end_clean();
 			echo 'success';
 		} elseif (!$sql) {
--- php/modules/stdHead.php
+++ php/modules/stdHead.php
@@ -28,7 +28,7 @@
 
 	// PHP files
 	define('PHP_EXPORT', $root_dir . 'php/exporting/export.php');
-	define('PHP_AJAX', $root_dir . 'php/modules/ajax.php');
+	define('PHP_AJAX', $root_dir . 'php/ajax.php');
 	define('PHP_BAND_LISTING', $root_dir . 'band-listing.php');
 
 	// IMG/GIF files
